57e116515afd3ec7cba1714a936690d2db3d6f04
brenosilvapedra <112491458+brenosilvapedra@users.noreply.github.com>
2024-07-01 11:02:28 -0300

    Add files via upload

---
+++ aninha/main.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternaticas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const resultado1 = document.querySelector(".caixa-resultado1");
+
+const perguntas = [
+    {
+        enunciado: "A festa junina é um evento relacionado ao folclore?",
+        alternativas: [
+            "Sim",
+            "Está comemoração que está presente todos os anos em escolas brasileiras é considerado parte do folclore"
+        ]
+    }
+]
